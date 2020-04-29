bf91badf974059b6cc391456a2a2c681e59ee43c
Jose D. Vasquez <jodavaga@gmail.com>
2020-04-28 20:10:34 -0500

    VoteApp - Persist data on local storage. save on localStorage only if Updated

--- src/components/votes/Votes.js
+++ src/components/votes/Votes.js
@@ -13,13 +13,34 @@ class Votes extends Component {
         }
     }
 
+    componentWillMount() {
+        if (localStorage.getItem('data')) {
+            console.log('data already exist');
+
+            const actualData = JSON.parse(localStorage.getItem('data'));
+            this.setState(prevState => {
+                prevState.cards = actualData
+            });
+        }
+
+    }
+
+    componentDidUpdate() {
+        const dataJson = JSON.stringify(this.state.cards);
+        console.warn('Updated!', dataJson);
+        // set data on localstorage
+        localStorage.setItem('data', dataJson);
+
+    }
+
+    // Add and edit votes count depending on the selected reaction.
     handleVote = (id, item) => {
         this.setState(prevState => {
             const updatedCards = prevState.cards.map(card => {
                 // voted on LIKE
                 if ( card.id == id && item === 'btn-like') {
                     card.likes = card.likes + 1;
-                    console.log('voted on:', id);
+                    // console.log('voted on:', id);
                 }
                 if ( card.id == id && item === 'btn-dislike') {
                     card.dislikes = card.dislikes + 1;
